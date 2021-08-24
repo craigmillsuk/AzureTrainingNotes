5c5305fc2ced9aa9dc9de1ab08be588026e1c648
Craig Mills <craig.mills@288group.com>
2021-08-24 17:21:02 +0100

    Part 3 and updated practical

--- Practicals/ServerlessFuncs/ServerlessFuncs/Function1.cs
+++ Practicals/ServerlessFuncs/ServerlessFuncs/Function1.cs
@@ -1,32 +1,104 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace ServerlessFuncs
 {
-    public static class Function1
+    public static class TodoApi
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        private static List<Todo> items = new List<Todo>();
+
+        [FunctionName( "CreateTodo" )]
+        public static async Task<IActionResult> CreateTodo(
+            [HttpTrigger( AuthorizationLevel.Anonymous, "post", Route = "todo" )]
+            HttpRequest req,
+            TraceWriter log )
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.Info( "Creating a new todo list item" );
+            string requestBody = await new StreamReader( req.Body ).ReadToEndAsync();
+            var input = JsonConvert.DeserializeObject<TodoCreateModel>( requestBody );
 
-            string num1 = req.Query["num1"];
-            string num2 = req.Query["num2"];
+            var todo = new Todo() { TaskDescription = input.TaskDescription };
+            items.Add( todo );
+            return new OkObjectResult( todo );
+        }
 
-            string responseMessage = string.IsNullOrEmpty(num1) || string.IsNullOrEmpty( num2 )
-                ? "Please pass a num1 and num2 parameter."
-                : $"Addition: {Convert.ToInt32(num1) + Convert.ToInt32(num2)}";
+        [FunctionName( "GetTodos" )]
+        public static IActionResult GetTodos(
+            [HttpTrigger( AuthorizationLevel.Anonymous, "get", Route = "todo" )]
+            HttpRequest req,
+            TraceWriter log )
+        {
+            log.Info( "Getting todo list items" );
+            return new OkObjectResult( items );
+        }
 
-            return new OkObjectResult(responseMessage);
+        [FunctionName( "GetTodoById" )]
+        public static IActionResult GetTodoById(
+            [HttpTrigger( AuthorizationLevel.Anonymous, "get", Route = "todo/{id}" )]
+            HttpRequest req,
+            TraceWriter log,
+            string id )
+        {
+            var todo = items.FirstOrDefault( t => t.Id == id );
+            if( todo == null )
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult( todo );
+        }
+
+        [FunctionName("UpdateTodo")]
+        public static async Task<IActionResult> UpdateTodo(
+            [HttpTrigger( AuthorizationLevel.Anonymous, "put", Route = "todo/{id}" )]
+            HttpRequest req,
+            TraceWriter log,
+            string id)
+        {
+            var todo = items.FirstOrDefault( t => t.Id == id );
+            if( todo == null )
+            {
+                return new NotFoundResult();
+            }
+
+            string requestBody = await new StreamReader( req.Body ).ReadToEndAsync();
+            var updated = JsonConvert.DeserializeObject<TodoUpdateModel>( requestBody );
+
+            todo.IsCompleted = updated.IsCompleted;
+            if( !string.IsNullOrEmpty( updated.TaskDescription ) )
+            {
+                todo.TaskDescription = updated.TaskDescription;
+            }
+
+            return new OkObjectResult(todo);
+        }
+
+        [FunctionName( "DeleteTodo" )]
+        public static IActionResult DeleteToDo(
+            [HttpTrigger( AuthorizationLevel.Anonymous, "delete", Route = "todo/{id}" )]
+            HttpRequest req,
+            TraceWriter log,
+            string id )
+        {
+            var todo = items.FirstOrDefault( t => t.Id == id );
+            if( todo == null )
+            {
+                return new NotFoundResult();
+            }
+
+            items.Remove( todo );
+            return new OkResult();
         }
     }
 }
---
+++ Practicals/ServerlessFuncs/ServerlessFuncs/Models.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerlessFuncs
+{
+    public class Todo
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("n");
+        public DateTime CreateDateTime { get; set; } = DateTime.UtcNow;
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    public class TodoCreateModel
+    {
+        public string TaskDescription { get; set; }
+    }
+
+    public class TodoUpdateModel
+    {
+        public bool IsCompleted { get; set; }
+        public string TaskDescription { get; set; }
+       
+    }
+}
